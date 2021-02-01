624de43c7d6914a9833d680ec3018fac31f568c9
Борисовец Олег <b_oleg.94@mail.ru>
2021-02-01 13:53:05 +0300

    1

--- src/book/admin.py
+++ src/book/admin.py
@@ -1,6 +1,35 @@
 from django.contrib import admin
 
-from .models import Author
+from .models import Author, Series, Genres, Publisher
 
-admin.site.register(Author)
-# Register your models here.
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = [
+        'pk',
+        'Author_name',
+        'Author_description'
+    ]
+
+class SeriesAdmin(admin.ModelAdmin):
+    list_display = [
+        'pk',
+        'series',
+        'series_description'
+    ]    
+
+class GenresAdmin(admin.ModelAdmin):
+    list_display = [
+        'pk',
+        'genres',
+        'genres_description'
+    ]       
+
+class PublisherAdmin(admin.ModelAdmin):
+    list_display = [
+        'pk',
+        'publisher',
+        'publisher_description'
+    ] 
+admin.site.register(Author, AuthorAdmin )
+admin.site.register(Series, SeriesAdmin)
+admin.site.register(Genres, GenresAdmin)
+admin.site.register(Publisher, PublisherAdmin)
--- src/book/models.py
+++ src/book/models.py
@@ -3,7 +3,56 @@ from django.db import models
 # Create your models here.
 # создаем табличку в которой будет 2 столбца первый имя автора второй описание 
 class Author(models.Model):
-    Author_neme = models.CharField("neme", max_length=50)
-    Author_description = models.CharField("description", max_length=200)
+    Author_name = models.CharField(
+        verbose_name="name", 
+        max_length=50)
+    Author_description = models.TextField(
+        verbose_name= "description",
+        null=True,
+        blank=True)
     def __str__(self):
-        return self.Author_neme
+        return self.Author_name
+# добовляем табличку с серия книг 2 поля первая какая серия книги  вторая описание серии .
+class Series(models.Model):
+    series =models.CharField(
+        verbose_name="Series",
+        max_length=20, 
+        blank=True,
+        null=True)
+    series_description=models.TextField(
+        verbose_name= "description",
+        null=True,
+        blank=True)
+    def __str__(self):
+        return self.series
+
+
+#Добовляем табличку жанр 
+
+class Genres(models.Model):
+    genres =models.CharField(
+        verbose_name="genres",
+        max_length=20, 
+        blank=True,
+        null=True)
+    genres_description=models.TextField(
+        verbose_name= "description",
+        null=True,
+        blank=True)
+    def __str__(self):
+        return self.genres        
+
+# Добавить издатесьство 
+
+class Publisher(models.Model):
+    publisher =models.CharField(
+        verbose_name="publisher",
+        max_length=50, 
+        blank=True,
+        null=True)
+    publisher_description=models.TextField(
+        verbose_name= "description",
+        null=True,
+        blank=True)
+    def __str__(self):
+        return self.publisher 
